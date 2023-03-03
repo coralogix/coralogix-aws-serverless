7a18796372e9cd3e94abd83169d0976c8eae16f5
juan-coralogix <juan@coralogix.com>
2023-03-03 16:27:19 -0300

    Secret Layer for Private Key

---
+++ src/lambda-secretLayer/layer/wrapper.js
@@ -0,0 +1,25 @@
+const assert = require("assert");
+
+async function getSecret() {
+  assert(process.env.SECRET_NAME, "No SECRET_NAME Variable!");
+  const AWS = require('aws-sdk');
+  
+  AWS.config.update({region:process.env.AWS_REGION});
+  const secretsManager = new AWS.SecretsManager();
+  const secretName = process.env.SECRET_NAME;
+  const params = {
+    SecretId: secretName
+  };
+  const secretData = await secretsManager.getSecretValue(params).promise();
+  const secretValue = secretData.SecretString;
+  const fs = require('fs');
+  fs.writeFile("/tmp/envVars", "export private_key=" + secretValue, function(err) {
+    if(err) {
+        return console.log(err);
+    }
+    //console.log("The file was saved!");
+}); 
+  
+}
+
+getSecret();
