c43ccb46d7ac0a82888ffc3bd92fee833f368bdd
guyrenny <127672145+guyrenny@users.noreply.github.com>
2023-06-29 16:05:20 +0300

    Create replace_codeuri.sh
    This file will be active from the Sync action to change the template and the README files of serverless integration for cloudformation integration

---
+++ replace_codeuri.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# This script will change the codeUri in a given template, so that the template will be validate
+# for cloudformation, and change the README file for that template.
+
+template_file=$1
+puckage_name=$2
+README_file=$3
+bucket_string="!Sub coralogix-serverless-repo-\${AWS::Region}"
+
+if grep -q "LambdaFunction" "$template_file"; then
+    yq eval '.Resources.LambdaFunction.Properties.CodeUri = {"Bucket": "'"$bucket_string"'", "S3Key": "'"$puckage_name"'.zip"}' -i $template_file
+    sed -i "s/'!Sub coralogix-serverless-repo-\${AWS::Region}/!Sub 'coralogix-serverless-repo-\${AWS::Region}/g" $template_file
+fi
+if grep -q "LambdaFunctionSSM" "$template_file"; then
+    yq eval '.Resources.LambdaFunctionSSM.Properties.CodeUri = {"Bucket": "'"$bucket_string"'", "S3Key": "'"$puckage_name"'.zip"}' -i $template_file
+    sed -i "s/'!Sub coralogix-serverless-repo-\${AWS::Region}/!Sub 'coralogix-serverless-repo-\${AWS::Region}/g" $template_file
+fi
+if grep -q "CustomResourceLambdaTriggerFunction" "$template_file"; then
+    yq eval '.Resources.CustomResourceLambdaTriggerFunction.Properties.CodeUri = {"Bucket": "'"$bucket_string"'", "S3Key": "helper.zip"}' -i $template_file
+    sed -i "s/'!Sub coralogix-serverless-repo-\${AWS::Region}/!Sub 'coralogix-serverless-repo-\${AWS::Region}/g" $template_file
+fi
+
+sed -i "1s/^/#Created automatically from coralogix\/coralogix-aws-serverless\n#Link to the repo: https:\/\/github.com\/coralogix\/coralogix-aws-serverless\/tree\/master\/src\/$puckage_name\n/" $template_file
+
+sed -i '/^## AWS Resource Manager Template Deployment$/,/^## Fields/c\
+## Fields 
+' $README_file
+
+sed -i '3s/^/This template were created automatically from coralogix\/coralogix-aws-serverless.\nTo make a change in the template go to the link below.\n\n/' $README_file
+
+sed -i "5s/^/\nhttps:\/\/github.com\/coralogix\/coralogix-aws-serverless\/tree\/master\/src\/$puckage_name\n/" $README_file
