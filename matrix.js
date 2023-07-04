68211580be23bda3828c7928a6315000ddf431b0
guyrenny <127672145+guyrenny@users.noreply.github.com>
2023-07-04 10:23:35 +0300

    Create Synchronizing.yml action to sync serverlees repo with cloudformation (#97)
    * Create Synchronizing.yml
    
    * Update Synchronizing.yml
    
    Add README sync
    
    * Create replace_codeuri.sh
    
    This file will be active from the Sync action to change the template and the README files of serverless integration for cloudformation integration
    
    * fix typo and change the wrokflow of matrix strategy, change replace_codeuri to add commants in the template and readme files
    
    Signed-off-by: guyrenny <guy.renny@coralogix.com>
    
    * Change the github token for the action
    
    ---------
    
    Signed-off-by: guyrenny <guy.renny@coralogix.com>

---
+++ matrix.js
@@ -0,0 +1,86 @@
+[ 
+    {
+      "name": "archive-reader",
+      "path": "src/archive-reader/**"
+    },
+    {
+      "name": "athena-s3",
+      "path": "src/athena-s3/**"
+    },
+    {
+      "name": "cloudtrail",
+      "path": "src/cloudtrail/**"
+    },
+    {
+      "name": "cloudtrail-sns",
+      "path": "src/cloudtrail-sns/**"
+    },
+    {
+      "name": "cloudwatch-logs",
+      "path": "src/cloudwatch-logs/**"
+    },
+    {
+      "name": "cloudwatch-metrics",
+      "path": "src/cloudwatch-metrics/**"
+    },
+    {
+      "name": "elasticsearch-reporter",
+      "path": "src/elasticsearch-reporter/**"
+    },
+    {
+      "name": "kafka",
+      "path": "src/kafka/**"
+    },
+    {
+      "name": "kinesis",
+      "path": "src/kinesis/**"
+    },
+    {
+      "name": "lambda-extension",
+      "path": "src/lambda-extension/**"
+    },
+    {
+      "name": "lambda-manager",
+      "path": "src/lambda-manager/**"
+    },
+    {
+      "name": "lambda-secretLayer",
+      "path": "src/lambda-secretLayer/**"
+    },
+    {
+      "name": "mongodb-atlas",
+      "path": "src/mongodb-atlas/**"
+    },
+    {
+      "name": "msk",
+      "path": "src/msk/**"
+    },
+    {
+      "name": "resource-metadata",
+      "path": "src/resource-metadata/**"
+    },
+    {
+      "name": "resource-tags",
+      "path": "src/resource-tags/**"
+    },
+    {
+      "name": "s3",
+      "path": "src/s3/**"
+    },
+    {
+      "name": "s3-sns",
+      "path": "src/s3-sns/**"
+    },
+    {
+      "name": "sf-eventlog",
+      "path": "src/sf-eventlog/**"
+    },
+    {
+      "name": "sns",
+      "path": "src/sns/**"
+    },
+    {
+      "name": "vpc-flow-logs",
+      "path": "src/vpc-flow-logs/**"
+    }
+   ]
