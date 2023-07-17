c98db7738a9817788e2cd78f582932e9b87ed929
guyrenny <127672145+guyrenny@users.noreply.github.com>
2023-07-17 20:27:43 +0300

    update the secret layer to support nodejs18 and nodejs16

---
+++ src/lambda-secretLayer/wrapper16.js
@@ -0,0 +1,28 @@
+const assert = require("assert");
+
+async function getSecret() {
+  //assert(process.env.SECRET_NAME, "No SECRET_NAME Variable!");
+
+  const AWS = require('aws-sdk');
+  
+  AWS.config.update({region:process.env.AWS_REGION});
+  const secretsManager = new AWS.SecretsManager();
+  //const secretName = process.env.SECRET_NAME;
+  const secretName = "lambda/coralogix/" + process.env.AWS_REGION + "/" + process.env.AWS_LAMBDA_FUNCTION_NAME;
+  console.log(secretName);
+  const params = {
+    SecretId: secretName
+  };
+  const secretData = await secretsManager.getSecretValue(params).promise();
+  const secretValue = secretData.SecretString;
+  const fs = require('fs');
+  fs.writeFile("/tmp/envVars", "export private_key=" + secretValue, function(err) {
+    if(err) {
+        return console.log(err);
+    }
+    //console.log("The file was saved!");
+}); 
+  
+}
+
+getSecret();
