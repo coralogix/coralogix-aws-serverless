7fa039282c730673d2c5ffc91a760cb6888b5554
guyrenny <127672145+guyrenny@users.noreply.github.com>
2023-07-18 19:05:50 +0300

    add extra line to wrapper.sh

--- src/lambda-secretLayer/wrapper.sh
+++ src/lambda-secretLayer/wrapper.sh
@@ -5,10 +5,12 @@ node_version=$(node -e "console.log(process.version)")
 if [[ $node_version == v18.* ]]; then
     node /opt/wrapper18.js
     #cat /tmp/envVars
+    
     source /tmp/envVars
 else
     node /opt/wrapper16.js
     #cat /tmp/envVars
+    
     source /tmp/envVars
 fi
 
