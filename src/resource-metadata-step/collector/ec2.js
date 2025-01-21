a9245e0b1086e0fe8b3802a9ad546a5926c1f423
Mikhail Chinkov <michael.chinkov@gmail.com>
2025-01-21 13:16:26 +0100

    split the function into two

---
+++ src/resource-metadata-step/collector/ec2.js
@@ -0,0 +1,80 @@
+import assert from 'assert'
+import { EC2Client, paginateDescribeInstances } from '@aws-sdk/client-ec2'
+import { schemaUrl, stringAttr } from '../generator/common.js'
+
+const validateAndExtractConfiguration = () => {
+    assert(process.env.RESOURCE_TTL_MINUTES, "RESOURCE_TTL_MINUTES env var missing!")
+    const resourceTtlMinutes = parseInt(process.env.RESOURCE_TTL_MINUTES, 10)
+    return { resourceTtlMinutes };
+};
+const { resourceTtlMinutes } = validateAndExtractConfiguration();
+
+const ec2Client = new EC2Client();
+
+export const collectEc2Resources = async (region, accountId) => {
+
+    console.info("Collecting EC2 instances")
+    const instanceResources = await collectEc2InstanceResources(region, accountId)
+
+    instanceResources.forEach((f, index) =>
+        console.debug(`Ec2Instance (${index + 1}/${instanceResources.length}): ${JSON.stringify(f)}`)
+    )
+
+    console.info(`Collected ${instanceResources.length} EC2 instances`)
+
+    return instanceResources
+}
+
+const collectEc2InstanceResources = async (region, accountId) => {
+    const instances = [];
+    for await (const page of paginateDescribeInstances({ client: ec2Client }, {})) {
+        if (page.Reservations) {
+            instances.push(...page.Reservations.flatMap(r => r.Instances));
+        }
+    }
+    return instances.map(i => makeEc2InstanceResource(i, region, accountId))
+}
+
+const makeEc2InstanceResource = (i, region, accountId) => {
+
+    const instanceId = i.InstanceId
+    const arn = `arn:aws:ec2:${region}:${accountId}:instance/${instanceId}`
+
+    const attributes = [
+        stringAttr("cloud.provider", "aws"),
+        stringAttr("cloud.platform", "aws_ec2"),
+        stringAttr("cloud.account.id", accountId),
+        stringAttr("cloud.region", region),
+        stringAttr("cloud.availability_zone", i.Placement?.AvailabilityZone),
+        stringAttr("cloud.resource_id", arn),
+        stringAttr("host.id", instanceId),
+        stringAttr("host.image.id", i.ImageId),
+        stringAttr("host.type", i.InstanceType),
+    ]
+
+    const name = i.Tags?.find(kv => kv.Key === "Name")?.Value
+    if (name) {
+        stringAttr("host.name", name)
+    }
+
+    attributes.push(...convertEc2TagsToAttributes(i.Tags))
+
+    return {
+        resourceId: arn,
+        resourceType: "aws:ec2:instance",
+        attributes,
+        schemaUrl,
+        resourceTtl: {
+            seconds: resourceTtlMinutes * 60,
+            nanos: 0,
+        },
+    }
+}
+
+// WARNING the tags data structure is different in lambda and in ec2
+const convertEc2TagsToAttributes = tags => {
+    if (!tags) {
+        return []
+    }
+    return tags.map(tag => stringAttr(`cloud.tag.${tag.Key}`, tag.Value));
+}
