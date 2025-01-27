75d7cf799935a23009e48a22a1030604bde97765
Mikhail Chinkov <michael.chinkov@gmail.com>
2025-01-27 12:18:03 +0100

    remove filters from generator

--- src/resource-metadata-sqs/generator/lambda.js
+++ src/resource-metadata-sqs/generator/lambda.js
@@ -1,6 +1,5 @@
 import assert from 'assert'
 import { LambdaClient, GetFunctionCommand, ListAliasesCommand, GetPolicyCommand, ListVersionsByFunctionCommand, ListEventSourceMappingsCommand } from '@aws-sdk/client-lambda'
-import { ResourceGroupsTaggingAPIClient, paginateGetResources } from '@aws-sdk/client-resource-groups-tagging-api';
 import { schemaUrl, extractArchitecture, intAttr, stringAttr, traverse, flatTraverse } from './common.js'
 
 const validateAndExtractConfiguration = () => {
@@ -10,15 +9,11 @@ const validateAndExtractConfiguration = () => {
     const resourceTtlMinutes = parseInt(process.env.RESOURCE_TTL_MINUTES, 10)
     assert(process.env.COLLECT_ALIASES, "COLLECT_ALIASES env var missing!")
     const collectAliases = String(process.env.COLLECT_ALIASES).toLowerCase() === "true"
-    const includeRegex = process.env.LAMBDA_FUNCTION_INCLUDE_REGEX_FILTER ? new RegExp(process.env.LAMBDA_FUNCTION_INCLUDE_REGEX_FILTER) : null
-    const excludeRegex = process.env.LAMBDA_FUNCTION_EXCLUDE_REGEX_FILTER ? new RegExp(process.env.LAMBDA_FUNCTION_EXCLUDE_REGEX_FILTER) : null
-    const tagFilters = process.env.LAMBDA_FUNCTION_TAG_FILTERS ? JSON.parse(process.env.LAMBDA_FUNCTION_TAG_FILTERS) : null
-    return { latestVersionsPerFunction, resourceTtlMinutes, collectAliases, includeRegex, excludeRegex, tagFilters };
+    return { latestVersionsPerFunction, resourceTtlMinutes, collectAliases };
 }
-const { latestVersionsPerFunction, resourceTtlMinutes, collectAliases, includeRegex, excludeRegex, tagFilters } = validateAndExtractConfiguration();
+const { latestVersionsPerFunction, resourceTtlMinutes, collectAliases } = validateAndExtractConfiguration();
 
 const lambdaClient = new LambdaClient();
-const resourceGroupsTaggingAPIClient = tagFilters ? new ResourceGroupsTaggingAPIClient() : null;
 
 // Helper function to normalize property access regardless of case
 const prop = (obj, key) => {
@@ -35,17 +30,6 @@ export const generateLambdaResources = async (functions) => {
         ...f
     }));
 
-    if (includeRegex) {
-        functions = functions.filter(f => includeRegex.test(f.functionArn))
-    }
-    if (excludeRegex) {
-        functions = functions.filter(f => !excludeRegex.test(f.functionArn))
-    }
-    if (tagFilters) {
-        const arns = new Set(await generateFunctionsArnsMatchingTagFilters());
-        functions = functions.filter(f => arns.has(f.functionArn))
-    }
-
     console.info("Generating function details")
     const { functionResources, aliasResources, versionsToCollect } = await generateFunctionAndAliasResources(functions)
 
@@ -57,18 +41,6 @@ export const generateLambdaResources = async (functions) => {
     console.info(`Generated ${functionResources.length} functions, ${functionVersionResources.length} function versions and ${aliasResources.length} aliases`)
 
     return resources
-}
-
-const generateFunctionsArnsMatchingTagFilters = async () => {
-    const input = {
-        ResourceTypeFilters: ['lambda:function'],
-        TagFilters: tagFilters,
-    };
-    const arns = [];
-    for await (const page of paginateGetResources({ client: resourceGroupsTaggingAPIClient }, input)) {
-        arns.push(...page.ResourceTagMappingList.map(r => r.ResourceARN));
-    }
-    return arns
 }
 
 const generateFunctionAndAliasResources = async (listOfFunctions) => {
