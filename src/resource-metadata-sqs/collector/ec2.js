4ab18fe00c991dadf01c97b413f15ff7dabc6dea
Mikhail Chinkov <michael.chinkov@gmail.com>
2025-02-17 11:54:54 +0100

    fix: rewrite batch collection of EC2 instances

--- src/resource-metadata-sqs/collector/ec2.js
+++ src/resource-metadata-sqs/collector/ec2.js
@@ -3,11 +3,18 @@ import { EC2Client, paginateDescribeInstances } from '@aws-sdk/client-ec2'
 const ec2Client = new EC2Client();
 
 export const collectEc2Resources = async function* () {
-    for await (const page of paginateDescribeInstances({ client: ec2Client }, { MaxItems: 50 })) {
+    console.info("Collecting list of EC2 instances");
+    const CHUNK_SIZE = 50;
+
+    for await (const page of paginateDescribeInstances({ client: ec2Client })) {
         if (page.Reservations) {
             const pageInstances = page.Reservations.flatMap(r => r.Instances);
-            if (pageInstances.length > 0) {
-                yield pageInstances;
+
+            // Chunk the instances array into groups of CHUNK_SIZE to avoid exceeding the SQS message size limit
+            for (let i = 0; i < pageInstances.length; i += CHUNK_SIZE) {
+                const chunk = pageInstances.slice(i, i + CHUNK_SIZE);
+                console.info(`Yielding chunk with ${chunk.length} instances`);
+                yield chunk;
             }
         }
     }
